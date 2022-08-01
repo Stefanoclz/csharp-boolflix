963833e02fb32c197b7154f7606b4572de4a5f44
Stefano Calzoni <stefanoclz@hotmail.it>
2022-08-01 14:17:25 +0200

    Aggiungere i file di progetto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace csharp_boolflix.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
